05442bccff6c4700ba71c972806add8900e34f72
MingyeomKim <12201863@office.inha.ac.kr>
2021-12-05 23:51:34 +0900

    12/05

---
+++ 5 Stacks, Queues, and Deques/postfix.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//  ABC+*와 같은 식을 정상적으로 연산하기
+void eval_postfix(const string &postfix) {
+
+}
+
+
+// 중위 표기식을 후위 표기식으로 변환
+void infix_to_postfix(const string& infix) {
+
+}
+
+
+int main() {
+
+}
